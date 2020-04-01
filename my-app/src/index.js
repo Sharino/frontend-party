4b33210a6a4449e3ae2a84b4e5c6eb57a3656846
sarunas.misiunas <sarunas.misiunas@geniusssports.com>
2020-04-01 21:04:47 +0300

    fixing IE11

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,11 @@
+import 'react-app-polyfill/ie11'; import 'react-app-polyfill/stable';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import './index.css';
 import { store } from './_helpers'
 import App from './App'
+
 
 ReactDOM.render(
   <Provider store={store}>
